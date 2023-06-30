f1d9a1af272e09f23c7c29529220f20978f65e2d
Ymapoff <kumar13-05@mail.ru>
2023-06-30 15:09:57 +0300

    com

--- src/test/java/cardTest.java
+++ src/test/java/cardTest.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -5,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 class cardTest {
 
@@ -27,9 +31,16 @@ class cardTest {
     }
 
     @Test
-    void shouldTestApplication() {
-        driver.get(";http://0.0.0.0:9999");
+    void shouldTestApplication() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class]");
+        form.$("[data-test-id=name] input").sendKeys("Морозов Павлик");
+        form.$("[data-test-id=phone] input").sendKeys("+79998887766");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
 
+
+        Thread.sleep(5000);
     }
 
 }
