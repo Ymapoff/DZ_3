e4c91a5da392bd0ac97ddb0cc0f7f6383d07198b
Ymapoff <kumar13-05@mail.ru>
2023-06-30 15:30:45 +0300

    com

--- src/test/java/cardTest.java
+++ src/test/java/cardTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class cardTest {
@@ -41,6 +43,22 @@ class cardTest {
 
 
         Thread.sleep(5000);
+    }
+
+    @Test
+    void invalidDataTest(){
+
+        open("http://localhost:9999");
+        SelenideElement form = $("[class]");
+        form.$("[data-test-id=name] input").sendKeys("Pupkina Aliona");
+        form.$("[data-test-id=phone] input").sendKeys("+79998887766");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+
+
     }
 
 }
