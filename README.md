bfc4456a54743b2634026d085f5233e46dc301f6
Ymapoff <kumar13-05@mail.ru>
2023-06-30 14:13:09 +0300

    com

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/n97hmupnrfia9qkp?svg=true)](https://ci.appveyor.com/project/Ymapoff/dz-3)
---
+++ src/test/java/cardTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class cardTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void shouldTestApplication() {
+        driver.get(";http://0.0.0.0:9999");
+
+    }
+
+}
